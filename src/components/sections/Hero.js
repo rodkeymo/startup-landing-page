f50335c2260d57c764cb229b9d25a223d6384b50
rodkeymo <keymro@gmail.com>
2022-11-29 11:58:26 +0300

    ..

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -65,7 +65,7 @@ const Hero = ({
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                All rounded landing page responsive to all devices once set up. Guaranteed beautification and excellence.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
